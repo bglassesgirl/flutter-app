91691aebae89d644ac5f5bcb63e471ec73c3bc8d
Luana Braga <luanabragagomes9@gmail.com>
2024-01-17 18:09:03 -0300

    feat: executing function after addPOstFrameCallBack

--- lib/homepage_statefull.dart
+++ lib/homepage_statefull.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamentos/homepage.dart';
 
 // tentar stf ou stl que ja cria as duas classes automaticamente
 
@@ -10,25 +11,42 @@ class HomePageStatefull extends StatefulWidget{
   }
 }
 
-//
+
 class _HomePageStateFullState  extends State<HomePageStatefull>{
   String texto = 'Estou no StateLessWidgtFull';
+
+  @override
+  void initState() {
+    super.initState();
+    texto = 'texto alterado pelo initState';
+
+    // permite chamarapenas quando a pagina estiver construida
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      print('addPostFrameCallBack');
+       Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => HomePageStateless(), ));
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
      return Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            Text(texto),
-            TextButton(
-              onPressed: (){
-              setState(() {
-                 texto = 'alterei o meu texto ;)';
-              });
-            }, child: Text('alterar texto'),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(texto),
+              TextButton(
+                onPressed: (){
+                setState(() {
+                   texto = 'alterei o meu texto ;)';
+                });
+              }, child: Text('alterar texto'),
+              ),
+            ],
+          ),
         ),
 
      );
