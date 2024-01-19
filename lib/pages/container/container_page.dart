04b659116f76446b6fc0517eb472f0979100e2ab
Luana Braga <luanabragagomes9@gmail.com>
2024-01-19 19:34:16 -0300

    feat: PopUpMenuButton

--- lib/pages/container/container_page.dart
+++ lib/pages/container/container_page.dart
@@ -7,25 +7,27 @@ class ContainerPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text('Container'),),
-           body: Container(
-              height: 200,
-              width: 200,
-              margin: const EdgeInsets.all(10),
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(30),
-                // ignore: prefer_const_literals_to_create_immutables
-                boxShadow: [
-                  const BoxShadow(
-                    color:Colors.black,
-                    blurRadius: 10,
-                    offset:Offset(10,10)
+           appBar: AppBar(title: const Text('Exemplo de Container'),),
+           body: Center(
+             child: Container(
+                height: 200,
+                width: 200,
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(30),
+                  // ignore: prefer_const_literals_to_create_immutables
+                  boxShadow: [
+                    const BoxShadow(
+                      color:Colors.black,
+                      blurRadius: 10,
+                      offset:Offset(10,10)
+                    ),
+                  ]
                   ),
-                ]
                 ),
-              ),
+           ),
        );
   }
 }
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 
+enum PopupMenuPages { container }
+
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        actions: [
+          PopupMenuButton<PopupMenuPages>(
+            tooltip: 'Escolha um item',
+            onSelected: (PopupMenuPages valueSelected) {
+              switch (valueSelected){
+                case PopupMenuPages.container:
+                  Navigator.of(context).pushNamed('/container');
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) {
+            return <PopupMenuItem<PopupMenuPages>>[
+              const PopupMenuItem<PopupMenuPages>(
+                value: PopupMenuPages.container,
+                child: Text('container')
+                )
+            ];
+          })
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
